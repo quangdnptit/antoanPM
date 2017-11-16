dcc51d561edd6c25d1f9c41721886e9d31437d4b
Keys <hangnt.ptit@gmail.com>
2017-11-17 00:19:22 +0700

    completed font end

--- antoanpm/admin/fontend/single-post.php
+++ antoanpm/admin/fontend/single-post.php
@@ -24,7 +24,49 @@ if(isset($_POST['btmPostCmt'])){
                 echo "<script type='text/javascript'>alert('Comment thành công!');</script>";
                 header('Location: http://localhost/antoanpm/admin/fontend/single-post.php?post_id='.$post_id.'&user_id='.$user_id.'');
             }else{
-                echo "<script type='text/javascript'>alert('Bạn phải đăng nhập để có thể bình luận trong bài viết này!');</script>";
+                echo "<script type='text/javascript'>alert('Comment thất bại!');</script>";
+            }
+        }
+    }else{
+        echo "<script type='text/javascript'>alert('Bạn phải đăng nhập để có thể bình luận trong bài viết này!');</script>";
+    }
+}
+if(isset($_POST['btmEditCmt'])){
+    if($_SESSION['user']){
+        $userId = isset($_POST['user'])?$_POST['user']: 0;
+        $comment_id = isset($_POST['comment_id'])?$_POST['comment_id']: 0;
+        $content = isset($_POST['content_edit'])?$_POST['content_edit']: '';
+        if(empty($content)){
+            echo "<script type='text/javascript'>alert('Ô bình luận không được bỏ trống!');</script>";
+        }else{
+            $data = array(
+                'id' => $comment_id,
+                'post_id' => $post_id,
+                'user_id' => $userId,
+                'derc' => $content
+            );
+            $result = $db->updateComment($data);
+            if($result){
+                echo "<script type='text/javascript'>alert('Sửa comment thành công!');</script>";
+                header('Location: http://localhost/antoanpm/admin/fontend/single-post.php?post_id='.$post_id.'&user_id='.$user_id.'');
+            }else{
+                echo "<script type='text/javascript'>alert('Sửa comment thất bại!');</script>";
+            }
+        }
+    }else{
+        echo "<script type='text/javascript'>alert('Bạn phải đăng nhập để có thể bình luận trong bài viết này!');</script>";
+    }
+}
+if(isset($_POST['btmDeleteCmt'])){
+    if($_SESSION['user']){
+        $comment_id = isset($_POST['comment_id'])?$_POST['comment_id']: 0;
+        if($comment_id > 0){
+            $result = $db->deleteComment($comment_id);
+            if($result){
+                echo "<script type='text/javascript'>alert('Xóa comment thành công!');</script>";
+                header('Location: http://localhost/antoanpm/admin/fontend/single-post.php?post_id='.$post_id.'&user_id='.$user_id.'');
+            }else{
+                echo "<script type='text/javascript'>alert('Xóa comment thất bại!');</script>";
             }
         }
     }else{
@@ -46,23 +88,35 @@ if(!empty($post)){
                 }
             ?>
         </div>
-        <div class="post_comment">
-            <button class="button button_comment" type="button">Post comment</button>
-            <form action="" method="post">
-                <table style="width: 90%; display: none;" class="table_commnent">
-                    <tr>
-                        <td>
-                            <textarea type="text" name="content_comment" placeholder="comment" required="required"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="submit" name="btmPostCmt" value="Comment" style="margin-top: 10px;" class="button"/>
-                        </td>
-                    </tr>
-                </table>
-            </form>
-        </div>
+        <?php
+            if($post['status'] == 1) {
+                ?>
+                <div class="post_comment">
+                    <button class="button button_comment" type="button">Post comment</button>
+                    <form action="" method="post">
+                        <table style="width: 90%; display: none;" class="table_commnent">
+                            <tr>
+                                <td>
+                                    <textarea type="text" name="content_comment" placeholder="comment"
+                                              required="required"></textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <input type="submit" name="btmPostCmt" value="Comment" style="margin-top: 10px;"
+                                           class="button"/>
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+
+                <?php
+            }else{
+                echo "Close post";
+            }
+        ?>
+
         <?php
             $comments = $db->getAllCommentByPostId($post_id);
             foreach (array_reverse($comments) as $comment):
@@ -86,7 +140,36 @@ if(!empty($post)){
                             $user = $_SESSION['user'];
                             if($user['id'] == $comment['user_id'] || $user['level'] == 3):
                         ?>
-                                <span style="margin-left: 80px;"><a href="#">edit</a>|<a href="#">delete</a></span>
+                                <span style="margin-left: 80px;">
+                                    <input type="button" value="edit" class="edit" style="float: left;margin-left: 80px;"/>
+                                    <form action="" method="post" id="delete_comment">
+                                        <input type="hidden" name="comment_id" value="<?= $comment['id']; ?>"/>
+                                        <input type="submit" name="btmDeleteCmt" value="delete" />
+                                    </form>
+                                <form action="" method="post">
+                                    <table style="width: 90%; display: none;" class="form_edit_commnent">
+                                        <tr>
+                                            <td>
+                                                <textarea type="text" name="content_edit" placeholder="comment" required="required" style="height: 70px;width: 300px;margin-left: 80px;margin-top: 10px;"><?= $comment['derc'] ?></textarea>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <input type="hidden" name="comment_id" value="<?= $comment['id']; ?>"/>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <input type="hidden" name="user" value="<?= $comment['user_id']; ?>"/>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <input type="submit" name="btmEditCmt" value="Edit" style="margin-top: 10px;margin-left: 80px;" class="Edit"/>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </form>
                         <?php
                             endif;
                         endif;
@@ -107,6 +190,15 @@ if(!empty($post)){
         jQuery('.button_comment').click(function () {
             jQuery('.table_commnent').show();
             jQuery('.button_comment').hide();
+        });
+        jQuery('.edit').click(function () {
+            jQuery('.form_edit_commnent').show();
+        });
+        jQuery('.delete').click(function () {
+            if(confirm("Bạn có muốn xóa comment của mình")){
+                jQuery('.delete_comment').submit();
+                jQuery('.delete_comment').submit();
+            }
         });
     });
 </script>
